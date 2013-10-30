c80076faa9dd7622c1902d40623a26909dd63b7d
Camilla Berglund <elmindreda@elmindreda.org>
2013-10-30 19:56:27 +0100

    Added more information to report.

--- src/diag.c
+++ src/diag.c
@@ -4,12 +4,13 @@
 #include <string.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "diag.h"
 
 static char* analysis = NULL;
 
-static void append_to_analysis(const char* format, ...)
+static void append(const char* format, ...)
 {
     va_list vl;
     char buffer[65536];
@@ -29,6 +30,26 @@ static void append_to_analysis(const char* format, ...)
         analysis = strdup(buffer);
 }
 
+static void append_separator(void)
+{
+    append("\r\n");
+}
+
+static const char* format_video_mode(const GLFWvidmode* mode)
+{
+    static char buffer[512];
+
+    sprintf(buffer,
+            "%i x %i x %i (%i %i %i) %i Hz",
+            mode->width, mode->height,
+            mode->redBits + mode->greenBits + mode->blueBits,
+            mode->redBits, mode->greenBits, mode->blueBits,
+            mode->refreshRate);
+
+    buffer[sizeof(buffer) - 1] = '\0';
+    return buffer;
+}
+
 char* analyze(void)
 {
     int i, monitorCount;
@@ -37,28 +58,39 @@ char* analyze(void)
     if (!glfwInit())
         return NULL;
 
-    append_to_analysis("GLFWDIAG compiled on " __DATE__ "\r\n");
-    append_to_analysis("GLFW %s\r\n", glfwGetVersionString());
+    append("GLFWDIAG compiled on " __DATE__ "\r\n");
+    append("GLFW %s\r\n", glfwGetVersionString());
 
     monitors = glfwGetMonitors(&monitorCount);
     for (i = 0;  i < monitorCount;  i++)
     {
-        int j, modeCount;
+        int j, xpos, ypos, widthMM, heightMM, dpi, modeCount;
         const GLFWvidmode* modes;
+        const int primary = (glfwGetPrimaryMonitor() == monitors[i]);
+        const GLFWvidmode* mode = glfwGetVideoMode(monitors[i]);
 
-        append_to_analysis("Monitor %i (%s)\r\n", i, glfwGetMonitorName(monitors[i]));
+        glfwGetMonitorPos(monitors[i], &xpos, &ypos);
+        glfwGetMonitorPhysicalSize(monitors[i], &widthMM, &heightMM);
 
+        append_separator();
+        append("%s monitor %i (%s)\r\n",
+               primary ? "Primary" : "Secondary",
+               i,
+               glfwGetMonitorName(monitors[i]));
+
+        append("Current mode: %s\r\n", format_video_mode(mode));
+        append("Virtual position: %i %i\r\n", xpos, ypos);
+
+        dpi = (int) ((float) mode->width * 25.4f / (float) widthMM);
+        append("Physical size: %i x %i mm (%i dpi)\r\n", widthMM, heightMM, dpi);
+
+        append("Modes:\r\n");
         modes = glfwGetVideoModes(monitors[i], &modeCount);
         for (j = 0;  j < modeCount;  j++)
-        {
-            append_to_analysis("%i: %i x %i, %i BPP (%i %i %i), %i Hz\r\n",
-                               j,
-                               modes[j].width, modes[j].height,
-                               modes[j].redBits + modes[j].greenBits + modes[j].blueBits,
-                               modes[j].redBits, modes[j].greenBits, modes[j].blueBits,
-                               modes[j].refreshRate);
-        }
+            append("%4i: %s\r\n", j, format_video_mode(modes + j));
     }
+
+    append_separator();
 
     for (i = GLFW_JOYSTICK_1;  i < GLFW_JOYSTICK_LAST;  i++)
     {
@@ -69,13 +101,13 @@ char* analyze(void)
             glfwGetJoystickAxes(i, &axisCount);
             glfwGetJoystickButtons(i, &buttonCount);
 
-            append_to_analysis("Joystick %i (%s): %i axes, %i buttons\r\n",
-                               i,
-                               glfwGetJoystickName(i),
-                               axisCount, buttonCount);
+            append("Joystick %i (%s): %i axes, %i buttons\r\n",
+                   i,
+                   glfwGetJoystickName(i),
+                   axisCount, buttonCount);
         }
         else
-            append_to_analysis("Joystick %i: not present\r\n", i);
+            append("Joystick %i: not present\r\n", i);
     }
 
     glfwTerminate();
